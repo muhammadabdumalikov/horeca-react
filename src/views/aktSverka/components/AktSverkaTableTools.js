7be9a46d503d0056997dd42d1da205f83b7efc09
ulugbek15 <ulugbekvaliyev1@gmail.com>
2024-03-12 12:40:17 +0500

    commited

--- src/views/aktSverka/components/AktSverkaTableTools.js
+++ src/views/aktSverka/components/AktSverkaTableTools.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Button } from 'components/ui'
 import { HiOutlineDownload } from 'react-icons/hi'
 import FakturaTableSearch from './AktSerkaTableSearch'
-import { useSelector, useDispatch } from 'react-redux'
+import {  useDispatch } from 'react-redux'
 import { setDeleteMode } from '../store/stateSlice'
-import DatePickerRange from 'components/ui/DatePicker/DatePickerRange'
-import { setStartDate, setEndDate } from '../store/stateSlice'
+// import DatePickerRange from 'components/ui/DatePicker/DatePickerRange'
+// import { setStartDate, setEndDate } from '../store/stateSlice'
 
 const BatchDeleteButton = () => {
     const dispatch = useDispatch()
@@ -28,39 +28,39 @@ const BatchDeleteButton = () => {
 }
 
 const FakturaTableTools = () => {
-    const dateFormat = 'MMM DD, YYYY'
+    // const dateFormat = 'MMM DD, YYYY'
 
-    const dispatch = useDispatch()
+    // const dispatch = useDispatch()
 
-    const selectedRows = useSelector(
-        (state) => state.aktSverka.state.selectedRows
-    )
+    // const selectedRows = useSelector(
+    //     (state) => state.aktSverka.state.selectedRows
+    // )
 
-    const startDate = useSelector((state) => state.aktSverka.state.startDate)
-    const endDate = useSelector((state) => state.aktSverka.state.endDate)
+    // const startDate = useSelector((state) => state.aktSverka.state.startDate)
+    // const endDate = useSelector((state) => state.aktSverka.state.endDate)
 
-    const handleDateChange = (value) => {
-        dispatch(setStartDate(value[0]))
-        dispatch(setEndDate(value[1]))
+    // const handleDateChange = (value) => {
+    //     dispatch(setStartDate(value[0]))
+    //     dispatch(setEndDate(value[1]))
 
-        // dispatch(
-        //     getCustomers({
-        //         limit: pageSize,
-        //         offset: (pageIndex - 1) * pageSize + (pageIndex === 1 && 0),
-        //         role: 3,
-        //         from_date: dayjs(startDate).format('YYYY-MM-DD'),
-        //         to_date: dayjs(endDate).format('YYYY-MM-DD'),
-        //     })
-        // )
-    }
+    //     // dispatch(
+    //     //     getCustomers({
+    //     //         limit: pageSize,
+    //     //         offset: (pageIndex - 1) * pageSize + (pageIndex === 1 && 0),
+    //     //         role: 3,
+    //     //         from_date: dayjs(startDate).format('YYYY-MM-DD'),
+    //     //         to_date: dayjs(endDate).format('YYYY-MM-DD'),
+    //     //     })
+    //     // )
+    // }
 
     // console.log(startDate, endDate)
 
     return (
         <div className="flex flex-col lg:flex-row lg:items-center gap-4">
-            {selectedRows.length > 0 && startDate && endDate && (
+            {/* {selectedRows.length > 0 && startDate && endDate && ( */}
                 <BatchDeleteButton />
-            )}
+            {/* )} */}
             {/* <Link to="/data/order-list.csv" target="_blank" download>
                 <Button block size="sm" icon={<HiDownload />}>
                     Export
